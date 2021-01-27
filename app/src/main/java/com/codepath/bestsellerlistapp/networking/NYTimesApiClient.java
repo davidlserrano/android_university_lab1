fb7ffbb24b7313077df11867d14bf8d968bd00f9
David Serrano <davidserrano@Davids-iMac.attlocal.net>
2021-01-27 11:46:03 -0800

    updated API key

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -25,7 +25,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class NYTimesApiClient {
 
     // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "K844nnxp2MZTReCVH3rAbsGh4E2L7D0S";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
